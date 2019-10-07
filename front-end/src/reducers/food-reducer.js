13029d9d38147497cca78b69b25f9033529507af
leyla li <leylali@qq.com>
2019-10-07 08:12:14 -0700

    done with practice

--- front-end/src/reducers/food-reducer.js
+++ front-end/src/reducers/food-reducer.js
@@ -2,19 +2,19 @@ export default (state = [], {type, payload}) => {
   switch(type){
     case 'FOOD_LOAD':
       return payload;
-    // case 'FOOD_ADD':
-    //   return [...state, payload];
-    // case 'FOOD_UPDATE':
-    //   state = state.map(food=>{
-    //     if( food.id === payload.id ){
-    //       return payload;  
-    //     }
-    //     return food;
-    //   });
-    //   return state;
-    // case 'FOOD_DELETE':
-    //   state = state.filter(food => food.id !== payload);
-    //   return state;
+    case 'FOOD_ADD':
+      return [...state, payload];
+    case 'FOOD_UPDATE':
+      state = state.map(food=>{
+        if( food.id === payload.id ){
+          return payload;  
+        }
+        return food;
+      });
+      return state;
+    case 'FOOD_DELETE':
+      state = state.filter(food => food.id !== payload);
+      return state;
     default:
       return state;
   }
